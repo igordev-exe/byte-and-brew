e6c377cfa0118b16a433a308d5f1653eff624570
André Alves <andrealvesmendes2006@gmail.com>
2026-06-28 16:37:30 -0300

    Adicionando A exceção Estoque Insuficiente

--- src/br/edu/cafeteria/excecao/EstoqueInsuficienteException.java
+++ src/br/edu/cafeteria/excecao/EstoqueInsuficienteException.java
@@ -1,7 +1,31 @@
 package br.edu.cafeteria.excecao;
 
 public class EstoqueInsuficienteException extends Exception {
-    public EstoqueInsuficienteException(String mensagem) {
-        super(mensagem);
+
+    private final String codigoProduto;
+    private final int quantidadeSolicitada;
+    private final int quantidadeDisponivel;
+
+    public EstoqueInsuficienteException(String codigoProduto,
+                                         int quantidadeSolicitada,
+                                         int quantidadeDisponivel) {
+        super(String.format(
+                "Estoque insuficiente para o produto '%s': solicitado %d, disponível %d.",
+                codigoProduto, quantidadeSolicitada, quantidadeDisponivel));
+        this.codigoProduto = codigoProduto;
+        this.quantidadeSolicitada = quantidadeSolicitada;
+        this.quantidadeDisponivel = quantidadeDisponivel;
     }
-}/*a*/
+
+    public String getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public int getQuantidadeSolicitada() {
+        return quantidadeSolicitada;
+    }
+
+    public int getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
+    }
+}
