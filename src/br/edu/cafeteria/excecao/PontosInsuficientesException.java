5f16343a6d87192940eaa14d1f90555307c99ccf
André Alves <andrealvesmendes2006@gmail.com>
2026-06-28 16:38:08 -0300

    Adicionando a exceção Pontos Insuficientes

--- src/br/edu/cafeteria/excecao/PontosInsuficientesException.java
+++ src/br/edu/cafeteria/excecao/PontosInsuficientesException.java
@@ -1,7 +1,31 @@
 package br.edu.cafeteria.excecao;
 
 public class PontosInsuficientesException extends Exception {
-    public PontosInsuficientesException(String mensagem) {
-        super(mensagem);
+
+    private final String cpfCliente;
+    private final double saldoAtualXp;
+    private final double saldoNecessarioXp;
+
+    public PontosInsuficientesException(String cpfCliente,
+                                         double saldoAtualXp,
+                                         double saldoNecessarioXp) {
+        super(String.format(
+                "Pontos de XP insuficientes para o cliente '%s': saldo atual %.1f, necessário %.1f.",
+                cpfCliente, saldoAtualXp, saldoNecessarioXp));
+        this.cpfCliente = cpfCliente;
+        this.saldoAtualXp = saldoAtualXp;
+        this.saldoNecessarioXp = saldoNecessarioXp;
     }
-}/*a*/
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public double getSaldoAtualXp() {
+        return saldoAtualXp;
+    }
+
+    public double getSaldoNecessarioXp() {
+        return saldoNecessarioXp;
+    }
+}
