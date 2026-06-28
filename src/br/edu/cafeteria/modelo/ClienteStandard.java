5d8a1c9eb84b826a9b5367128df255623aaf9ac1
Igor Alves Rodrigues <igor12dev@gmail.com>
2026-06-28 13:52:12 -0300

    Extend Cliente class in ClienteStandard

--- src/br/edu/cafeteria/modelo/ClienteStandard.java
+++ src/br/edu/cafeteria/modelo/ClienteStandard.java
@@ -1,5 +1,25 @@
 package br.edu.cafeteria.modelo;
 
-public class ClienteStandard {
+public class ClienteStandard extends Cliente {
 
+    public ClienteStandard(String nome, String cpf) {
+        super(nome, cpf);
+    }
+
+    @Override
+    public int getTaxaXP() {
+        return 1;
+    }
+
+    @Override
+    public int calcularXP(double valor) {
+      
+        return (int) (valor * getTaxaXP());
+    }
+
+
+    @Override
+    public String toString() {
+        return "[Standard] " + super.toString();
+    }
 }
